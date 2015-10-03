6bf11e04c549f1a6c7ad7c8662984ec7032decd3
Alexander Neumann <alexander@bumpern.de>
2015-10-03 21:53:25 +0200

    Parser complete, more samples

--- src/cmd/grobi/randr.go
+++ src/cmd/grobi/randr.go
@@ -3,24 +3,33 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
 )
 
+// Output encapsulates a physical output with detected modes.
 type Output struct {
-	Name       string
-	Modes      []Mode
-	Active     bool
+	Name      string
+	Modes     []Mode
+	Connected bool
 }
 
+// Mode is an output mode that may be active or default.
 type Mode struct {
-	Name  string
-	Rate string
+	Name    string
 	Default bool
-	Active bool
+	Active  bool
 }
 
+var (
+	// ErrNotModeLine is returned by ParseModeLine when the line doesn't match
+	// the format for a mode line.
+	ErrNotModeLine = errors.New("not a mode line")
+)
+
+// ParseOutputLine returns the output parsed from the string.
 func ParseOutputLine(line string) (Output, error) {
 	output := Output{}
 
@@ -38,9 +47,9 @@ func ParseOutputLine(line string) (Output, error) {
 
 	switch ws.Text() {
 	case "connected":
-		output.Active = true
+		output.Connected = true
 	case "disconnected":
-		output.Active = false
+		output.Connected = false
 	default:
 		return Output{}, fmt.Errorf("unknown state %q", ws.Text())
 	}
@@ -48,26 +57,99 @@ func ParseOutputLine(line string) (Output, error) {
 	return output, nil
 }
 
-// func ParseResolutionLine(line) Mode
+// ParseModeLine returns the mode parsed from the string.
+func ParseModeLine(line string) (mode Mode, err error) {
+	if !strings.HasPrefix(line, "  ") {
+		return Mode{}, ErrNotModeLine
+	}
 
+	ws := bufio.NewScanner(bytes.NewReader([]byte(line)))
+	ws.Split(bufio.ScanWords)
+
+	if !ws.Scan() {
+		return Mode{}, fmt.Errorf("line too short, mode name not found: %s", line)
+	}
+	mode.Name = ws.Text()
+
+	if !ws.Scan() {
+		return Mode{}, fmt.Errorf("line too short, no refresh rate found: %s", line)
+	}
+	rate := ws.Text()
+
+	if rate[len(rate)-1] == '+' {
+		mode.Default = true
+	}
+
+	if rate[len(rate)-2] == '*' {
+		mode.Active = true
+	}
+
+	// handle single-word "+", which happens when a mode is default but not active
+	if ws.Scan() && ws.Text() == "+" {
+		mode.Default = true
+	}
+
+	return mode, nil
+}
+
+// RandrParse returns the list of outputs parsed from the reader.
 func RandrParse(rd io.Reader) (outputs []Output, err error) {
 	ls := bufio.NewScanner(rd)
 
+	const (
+		StateStart = iota
+		StateOutput
+		StateMode
+	)
+
+	var (
+		state  = StateStart
+		output Output
+	)
+
+nextLine:
 	for ls.Scan() {
 		line := ls.Text()
-		fmt.Printf("line: %v\n", line)
 
-		if strings.HasPrefix(line, "Screen ") {
-			continue
+		for {
+			switch state {
+			case StateStart:
+				if strings.HasPrefix(line, "Screen ") {
+					state = StateOutput
+					continue nextLine
+				}
+				return nil, fmt.Errorf(`first line should start with "Screen", found: %v`, line)
+
+			case StateOutput:
+				output, err = ParseOutputLine(line)
+				if err != nil {
+					return nil, err
+				}
+				state = StateMode
+				continue nextLine
+
+			case StateMode:
+				mode, err := ParseModeLine(line)
+				if err == ErrNotModeLine {
+					outputs = append(outputs, output)
+					output = Output{}
+					state = StateOutput
+					continue
+				}
+
+				if err != nil {
+					return nil, err
+				}
+
+				output.Modes = append(output.Modes, mode)
+				continue nextLine
+			}
 		}
+	}
 
-		output, err := ParseOutputLine(line)
-		if err != nil {
-			return nil, err
-		}
-
+	if output.Name != "" {
 		outputs = append(outputs, output)
 	}
 
-	return nil, nil
+	return outputs, nil
 }
--- src/cmd/grobi/randr_test.go
+++ src/cmd/grobi/randr_test.go
@@ -10,7 +10,8 @@ var randrTestOutputs = []struct {
 	str     string
 	outputs []Output
 }{
-	{`Screen 0: minimum 320 x 200, current 3280 x 1200, maximum 8192 x 8192
+	{
+		`Screen 0: minimum 320 x 200, current 3280 x 1200, maximum 8192 x 8192
 LVDS1 connected (normal left inverted right x axis y axis)
    1366x768      60.10 +
    1024x768      60.00
@@ -34,29 +35,149 @@ DP3 disconnected (normal left inverted right x axis y axis)`,
 		[]Output{
 			Output{
 				Name: "LVDS1",
-				Modes: []string{
-					"1366x768",
-					"1024x768",
-					"800x600",
-					"640x480",
+				Modes: []Mode{
+					{Name: "1366x768", Default: true},
+					{Name: "1024x768"},
+					{Name: "800x600"},
+					{Name: "640x480"},
 				},
+				Connected: true,
 			},
+			Output{Name: "VGA1"},
+			Output{Name: "HDMI1"},
+			Output{Name: "DP1"},
 			Output{
 				Name: "HDMI2",
-				Modes: []string{
-					"1600x1200",
-					"1280x1024",
-					"1280x960",
-					"1152x864",
-					"1024x768",
-					"832x624",
-					"800x600",
-					"640x480",
-					"720x400",
+				Modes: []Mode{
+					{Name: "1600x1200", Default: true, Active: true},
+					{Name: "1280x1024"},
+					{Name: "1280x960"},
+					{Name: "1152x864"},
+					{Name: "1024x768"},
+					{Name: "832x624"},
+					{Name: "800x600"},
+					{Name: "640x480"},
+					{Name: "720x400"},
 				},
-				Active:     true,
-				ActiveMode: "1600x1200",
+				Connected: true,
 			},
+			Output{Name: "DP2"},
+			Output{Name: "DP3"},
+		},
+	},
+	{
+		`Screen 0: minimum 320 x 200, current 3280 x 1200, maximum 8192 x 8192
+LVDS1 connected (normal left inverted right x axis y axis)
+   1366x768      60.10 +
+   1024x768      60.00
+   800x600       60.32    56.25
+   640x480       59.94`,
+		[]Output{
+			Output{
+				Name: "LVDS1",
+				Modes: []Mode{
+					{Name: "1366x768", Default: true},
+					{Name: "1024x768"},
+					{Name: "800x600"},
+					{Name: "640x480"},
+				},
+				Connected: true,
+			},
+		},
+	},
+	{
+		`Screen 0: minimum 8 x 8, current 4480 x 1440, maximum 32767 x 32767
+eDP1 connected 1920x1080+2560+0 (normal left inverted right x axis y axis) 276mm x 156mm
+   1920x1080     60.04*+
+   1400x1050     59.98
+   1600x900      60.00
+   1280x1024     60.02
+   1280x960      60.00
+   1368x768      60.00
+   1280x720      60.00
+   1024x768      60.00
+   1024x576      60.00
+   960x540       60.00
+   800x600       60.32    56.25
+   864x486       60.00
+   640x480       59.94
+   720x405       60.00
+   640x360       60.00
+DP1 disconnected (normal left inverted right x axis y axis)
+DP2 disconnected (normal left inverted right x axis y axis)
+DP2-1 disconnected (normal left inverted right x axis y axis)
+DP2-2 connected primary 2560x1440+0+0 (normal left inverted right x axis y axis) 597mm x 336mm
+   2560x1440     59.95*+
+   2048x1152     60.00
+   1920x1200     59.88
+   1920x1080     60.00    50.00    59.94    30.00    25.00    24.00    29.97    23.98
+   1600x1200     60.00
+   1680x1050     59.95
+   1280x1024     75.02    60.02
+   1200x960      59.99
+   1152x864      75.00
+   1280x720      60.00    50.00    59.94
+   1024x768      75.08    60.00
+   800x600       75.00    60.32
+   720x576       50.00
+   720x480       60.00    59.94
+   640x480       75.00    60.00    59.94
+   720x400       70.08
+DP2-3 disconnected (normal left inverted right x axis y axis)
+HDMI1 disconnected (normal left inverted right x axis y axis)
+HDMI2 disconnected (normal left inverted right x axis y axis)
+VIRTUAL1 disconnected (normal left inverted right x axis y axis)`,
+		[]Output{
+			Output{
+				Name: "eDP1",
+				Modes: []Mode{
+					{Name: "1920x1080", Default: true, Active: true},
+					{Name: "1400x1050"},
+					{Name: "1600x900"},
+					{Name: "1280x1024"},
+					{Name: "1280x960"},
+					{Name: "1368x768"},
+					{Name: "1280x720"},
+					{Name: "1024x768"},
+					{Name: "1024x576"},
+					{Name: "960x540"},
+					{Name: "800x600"},
+					{Name: "864x486"},
+					{Name: "640x480"},
+					{Name: "720x405"},
+					{Name: "640x360"},
+				},
+				Connected: true,
+			},
+			Output{Name: "DP1"},
+			Output{Name: "DP2"},
+			Output{Name: "DP2-1"},
+			Output{
+				Name: "DP2-2",
+				Modes: []Mode{
+					{Name: "2560x1440", Default: true, Active: true},
+					{Name: "2048x1152"},
+					{Name: "1920x1200"},
+					{Name: "1920x1080"},
+					{Name: "1600x1200"},
+					{Name: "1680x1050"},
+					{Name: "1280x1024"},
+					{Name: "1200x960"},
+					{Name: "1152x864"},
+					{Name: "1280x720"},
+					{Name: "1024x768"},
+					{Name: "800x600"},
+					{Name: "720x576"},
+					{Name: "720x480"},
+					{Name: "640x480"},
+					{Name: "720x400"},
+				},
+				Connected: true,
+			},
+			Output{Name: "DP2-3"},
+			Output{Name: "HDMI1"},
+			Output{Name: "HDMI2"},
+			Output{Name: "VIRTUAL1"},
 		},
 	},
 }
@@ -75,8 +196,24 @@ func TestRandrParse(t *testing.T) {
 			continue
 		}
 
-		if !reflect.DeepEqual(out, test.outputs) {
-			t.Errorf("result is not equal to expected result")
+		for i := range test.outputs {
+			out1 := test.outputs[i]
+			out2 := out[i]
+
+			if out1.Name != out2.Name {
+				t.Errorf("output %d: name not equal: want %q, got %q", i,
+					out1.Name, out2.Name)
+			}
+
+			if out1.Connected != out2.Connected {
+				t.Errorf("output %d: connected not equal: want %v, got %v", i,
+					out1.Connected, out2.Connected)
+			}
+
+			if !reflect.DeepEqual(out1.Modes, out2.Modes) {
+				t.Errorf("output %d: list of modes not equal: want %v, got %v", i,
+					out1.Modes, out2.Modes)
+			}
 		}
 	}
 }
@@ -88,15 +225,15 @@ var TestOutputLines = []struct {
 	{
 		"LVDS1 connected (normal left inverted right x axis y axis)",
 		Output{
-			Name:   "LVDS1",
-			Active: true,
+			Name:      "LVDS1",
+			Connected: true,
 		},
 	},
 	{
 		"VGA1 disconnected (normal left inverted right x axis y axis)",
 		Output{
-			Name:   "VGA1",
-			Active: false,
+			Name:      "VGA1",
+			Connected: false,
 		},
 	},
 }
@@ -111,6 +248,60 @@ func TestParseOutputLine(t *testing.T) {
 
 		if !reflect.DeepEqual(out, test.output) {
 			t.Errorf("test %d failed: expected Output not found", i)
+			continue
+		}
+	}
+}
+
+var TestModeLines = []struct {
+	line string
+	mode Mode
+}{
+	{
+		"  1152x864      75.00",
+		Mode{
+			Name: "1152x864",
+		},
+	},
+	{
+		"  1024x768      75.08    70.07    60.00",
+		Mode{
+			Name: "1024x768",
+		},
+	},
+	{
+		"  1600x1200     60.00*+",
+		Mode{
+			Name:    "1600x1200",
+			Active:  true,
+			Default: true,
+		},
+	},
+	{
+		"  1366x768      60.10 +",
+		Mode{
+			Name:    "1366x768",
+			Default: true,
+		},
+	},
+	{
+		"  832x624       74.55",
+		Mode{
+			Name: "832x624",
+		},
+	},
+}
+
+func TestParseModeLine(t *testing.T) {
+	for i, test := range TestModeLines {
+		mode, err := ParseModeLine(test.line)
+		if err != nil {
+			t.Errorf("test %d returned error: %v", i, err)
+			continue
+		}
+
+		if !reflect.DeepEqual(mode, test.mode) {
+			t.Errorf("test %d failed: expected Mode not found", i)
 			continue
 		}
 	}
