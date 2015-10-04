576d9b13d0f523feced820d285b26e03d8c8d4c2
Alexander Neumann <alexander@bumpern.de>
2015-10-04 20:24:30 +0200

    Add command `version`

---
+++ src/cmd/grobi/cmd_version.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var version = "compiled manually"
+var compiledAt = "unknown time"
+
+type CmdVersion struct{}
+
+func init() {
+	_, err := parser.AddCommand("version",
+		"display version",
+		"The version command displays detailed information about the version",
+		&CmdVersion{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (cmd CmdVersion) Execute(args []string) error {
+	fmt.Printf("restic %s\ncompiled at %s with %v\n",
+		version, compiledAt, runtime.Version())
+
+	return nil
+}
